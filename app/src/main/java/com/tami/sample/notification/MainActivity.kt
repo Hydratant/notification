d45d3d498d45e11ed3a0ff3ea3b78a6c08f0f046
상혁 <jgps3505@gmail.com>
2019-12-05 16:02:35 +0900

    Default Notification Sample

--- app/src/main/java/com/tami/sample/notification/MainActivity.kt
+++ app/src/main/java/com/tami/sample/notification/MainActivity.kt
@@ -2,11 +2,24 @@ package com.tami.sample.notification
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tami.notification.createNotification
+import com.tami.notification.sendNotification
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        defaultNotification()
+    }
+
+    // DefaultNotification
+    private fun defaultNotification() {
+        val title = "Test"
+        val message = "Message"
+        val smallIcon = R.mipmap.ic_launcher
+
+        val notification = this.createNotification(title = title, message = message, smallIcon = smallIcon)
+        this.sendNotification(1, notification)
     }
 }
