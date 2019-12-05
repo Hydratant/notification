c7d1be21daf1eb6e0cf9fe702cd8d029cd69ec9d
상혁 <jgps3505@gmail.com>
2019-12-05 15:59:13 +0900

    Notification 설명 주석

--- app/src/main/java/com/tami/sample/notification/MyApplication.kt
+++ app/src/main/java/com/tami/sample/notification/MyApplication.kt
@@ -1,13 +1,14 @@
 package com.tami.sample.notification
 
 import android.app.Application
-import com.tami.notification.createNotificationChannel
+import com.tami.notification.initDefaultNotificationChannel
 
 class MyApplication : Application() {
 
-
     override fun onCreate() {
         super.onCreate()
-        createNotificationChannel()
+
+        // 간편하게 Default Channle을 생성하고 싶을 때 사용
+        initDefaultNotificationChannel()
     }
 }
--- notification/src/main/java/com/tami/notification/NotificationExt.kt
+++ notification/src/main/java/com/tami/notification/NotificationExt.kt
@@ -1,3 +1,5 @@
+@file:Suppress("SpellCheckingInspection")
+
 package com.tami.notification
 
 import android.annotation.TargetApi
@@ -18,6 +20,14 @@ const val NOTIFICATION_CHANNEL_NAME = "기본"
  *  @param channelId : Notification ChannelId를 설정한다  ( Default : Default )
  *  @param channelName : Notification ChannelName을 설정한다 ( Default : 기본 )
  *  @param importance : Notification의 중요도를 설정한다 ( NONE, MIN, LOW, DEFAULT, HIGH, MAX / Default : DEFAULT)
+ *  @param groupId : Notification Ground을 설정하는 ID
+ *  @param description : 채널 설명 최대길이 300자
+ *  @param isLights : 알림이 왔을 때 Light기능이 있는 장치에서 Light를 보여줄것인지 정한다.
+ *  @param lightColor : 알림이 왔을 때 Light 기능이 true로 되어 있을 시 Light의 색깔을 정한다. ex) Color.Green
+ *  @param vibration : 알림이 왔을 때 진동여부를 정한다.
+ *  @param vibrationPattern : 알림이 왔을 때 진동 패턴을 정한다. ex) val vibration = longArrayOf(100,200,100,200)
+ *  @param screenVisibility : 알림이 왔을 때 잠금화면에서 보여줄 여부를 정한다. Default : Public
+ *  @param isShowBadge : 알림이 왔을 때 Badge Show 여부 설정
  *
  */
 @TargetApi(Build.VERSION_CODES.O)
@@ -27,22 +37,26 @@ fun Application.createNotificationChannel(
         importance: Int = NotificationManager.IMPORTANCE_DEFAULT,
         groupId: String? = null,
         description: String? = null,
-        lights: Boolean? = null,
+        isLights: Boolean? = null,
         lightColor: Int? = null,
         vibration: Boolean? = null,
         vibrationPattern: LongArray? = null,
-        isShowBadge: Boolean = true,
-        screenVisibility: Int = NotificationCompat.VISIBILITY_PUBLIC) {
+        screenVisibility: Int = NotificationCompat.VISIBILITY_PUBLIC,
+        isShowBadge: Boolean = true) {
 
     // Android O 이상 가능
     if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) return
+
+    // 해당 Channel이 생성되어있으면 생성 로직을 타지 않는다.
+    val isCreateChannel = isNotificationChannel(channelId)
+    if (isCreateChannel) return
 
     val context = applicationContext
     val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
     val notificationChannel = NotificationChannel(channelId, channelName, importance).apply {
         if (groupId != null) group = groupId                                // Channel Group 설정
         if (description != null) setDescription(description)                // 채널 설명 최대길이 300자
-        if (lights != null) enableLights(lights)                            // 알림이 왔을 때 Light기능이 있는 장치에서 Light를 보여줄것인지 정한다.
+        if (isLights != null) enableLights(isLights)                        // 알림이 왔을 때 Light기능이 있는 장치에서 Light를 보여줄것인지 정한다.
         if (lightColor != null) setLightColor(lightColor)                   // 알림이 왔을 때 Light 기능이 true로 되어 있을 시 Light의 색깔을 정한다. ex) Color.Green
         if (vibration != null) enableVibration(vibration)                   // 알림이 왔을 때 진동여부를 정한다.
         if (vibrationPattern != null) setVibrationPattern(vibrationPattern) // 알림이 왔을 때 진동 패턴을 정한다. ex) val vibration = longArrayOf(100,200,100,200)
@@ -52,6 +66,11 @@ fun Application.createNotificationChannel(
     notificationManager.createNotificationChannel(notificationChannel)
 }
 
+/**
+ * NotificationChannel 생성 여부를 체크한다.
+ *
+ * @return true : 채널이 하나라도 생성되어있음 , false : 채널이 하나도 생성되어있지않음.
+ */
 @TargetApi(Build.VERSION_CODES.O)
 fun Context.isNotificationChannels(): Boolean {
 
@@ -65,6 +84,13 @@ fun Context.isNotificationChannels(): Boolean {
     return true
 }
 
+/**
+ * 해당 NotificationChannel 생성 여부를 체크한다.
+ *
+ * @param channelId : 체크할 ChannelId
+ * @return true : 해당채널이 생성되어있다 / false : 해당채널이 생성되어있지 않다.
+ *
+ */
 @TargetApi(Build.VERSION_CODES.O)
 fun Context.isNotificationChannel(channelId: String): Boolean {
 
@@ -81,6 +107,12 @@ fun Context.isNotificationChannel(channelId: String): Boolean {
     return true
 }
 
+/**
+ * 해당 NotificationChannel을 삭제한다.
+ *
+ *  @param channelId : 삭제 할 ChannelId
+ *
+ */
 @TargetApi(Build.VERSION_CODES.O)
 fun Context.deleteNotificationChannel(channelId: String) {
     if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) return
@@ -88,6 +120,12 @@ fun Context.deleteNotificationChannel(channelId: String) {
     notificationManger.deleteNotificationChannel(channelId)
 }
 
+/**
+ *  Noticiation Group을 만든다.
+ *
+ *  @param id : 해당 Group id
+ *  @param name : 해당 Group Name
+ */
 @TargetApi(Build.VERSION_CODES.O)
 fun Context.createNotificationGroup(id: String, name: CharSequence) {
     if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) return
@@ -96,6 +134,26 @@ fun Context.createNotificationGroup(id: String, name: CharSequence) {
     notificationManger.createNotificationChannelGroup(notificationChannelGroup)
 }
 
+/**
+ *
+ * Notification을 생성한다.
+ *
+ * @param channelId : 생성할 Notification의 ChannelId를 정한다.
+ * @param title : Notification의 Title
+ * @param message : Notification의 Message
+ * @param priority : Notification의 중요도를 정한다. ( Oreo 이상은 Channel priority를 따라간다. )
+ * @param autoCancel : 알림 클릭 시 자동으로 Notification Cancel 여부를 정한다.
+ * @param sound : 알림이 왔을 때 소리 설정
+ * @param default : 알림이 왔을 때 소리, 진동, 불빛 설정 ( Default NotificationCompat.DEFAULT_ALL )
+ * @param badgeIcon : Badge Icon Type ( Default NotificationCompat.BADGE_ICON_NONE )
+ * @param smallIcon : Notification 의 Icon
+ * @param largeIcon : Notification 의 LargeIcon
+ * @param number : 읽지 않은 알림 갯수를 설정한다.
+ * @param contentIntent : 알림을 클릭했을 때 Intent 설정
+ *
+ * @return 해당 옵션으로 생성 된 Notification
+ *
+ */
 fun Context.createNotification(channelId: String = NOTIFICATION_CHANNEL_ID,
                                title: CharSequence,
                                message: CharSequence,
@@ -104,7 +162,7 @@ fun Context.createNotification(channelId: String = NOTIFICATION_CHANNEL_ID,
                                sound: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION),
                                default: Int = NotificationCompat.DEFAULT_ALL,
                                badgeIcon: Int = NotificationCompat.BADGE_ICON_NONE,
-                               smallIcon: Int? = null,
+                               smallIcon: Int,
                                largeIcon: Bitmap? = null,
                                number: Int? = null,
                                contentIntent: PendingIntent? = null): Notification {
@@ -117,18 +175,29 @@ fun Context.createNotification(channelId: String = NOTIFICATION_CHANNEL_ID,
         setSound(sound)                                             // 알림이 왔을 때 소리
         setDefaults(default)                                        // 알림이 왔을 때 소리, 진동, 불빛 ( Default NotificationCompat.DEFAULT_ALL )
         setBadgeIconType(badgeIcon)                                 // Badge Icon Type ( Default NotificationCompat.BADGE_ICON_NONE )
-        if (smallIcon != null) setSmallIcon(smallIcon)              // 알림의 제목 옆 작은 아이콘
+        setSmallIcon(smallIcon)                                     // 알림의 제목 옆 작은 아이콘
         if (largeIcon != null) setLargeIcon(largeIcon)              // 알림의 큰 아이콘
         if (number != null) setNumber(number)                       // 읽지 않은 알림 갯수를 설정한다.
         if (contentIntent != null) setContentIntent(contentIntent)  // 알림을 클릭했을때 보내는 Intent 설정
     }.build()
 }
 
+/**
+ *
+ * Notification을 보낸다.
+ *
+ * @param notificationId : 해당 NotificationId
+ * @param notification : 보낼 Notification
+ *
+ */
 fun Context.sendNotification(notificationId: Int, notification: Notification) {
     val notificationManger = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
     notificationManger.notify(notificationId, notification)
 }
 
+/**
+ * DefaultNotificationChannel을 생성한다.
+ */
 @TargetApi(Build.VERSION_CODES.O)
 fun Application.initDefaultNotificationChannel(
         importance: Int = NotificationManager.IMPORTANCE_DEFAULT) {
